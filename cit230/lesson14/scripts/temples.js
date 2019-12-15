72b6e307967067079a05a512d1169b0721b22fe2
chloehollman <chollman@byui.edu>
2019-12-14 21:00:05 -0700

    finished website

---
+++ cit230/lesson14/scripts/temples.js
@@ -0,0 +1,15 @@
+fetch ('json/temples.json')
+    .then(
+        (response) => response.json()
+    )
+    .then(
+        (temples) => {
+                temples.forEach(
+                    temple => {
+                        console.log(temple);
+                    }
+                )
+            }
+    )
+
+
